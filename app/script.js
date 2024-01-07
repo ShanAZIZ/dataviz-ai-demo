b9b3adaa556c02c76a43d474a37edbc9e9b58225
saziz <shan.azizh@gmail.com>
2024-01-07 16:01:56 +0100

    initial for start

--- app/script.js
+++ app/script.js
@@ -1,10 +1,15 @@
-document.getElementById('csvFile').addEventListener('change', generateCharts)
+document.getElementById('csvFile').addEventListener('change', onAddFile)
 
-async function generateCharts(event) {
-    const file = event.target.files[0]
-    const stringData = await file.text();
-    const charts = await callOpenAI(prepareStringData(stringData))
-    createCharts(charts)
+/**
+ * Cette fonction est appeller lors de l'insertion d'un fichier
+ * Il faut recuperer le contenu du fichier sous forme de texte
+ * Reduire le texte en utilisant la fonction prepareStringData
+ * Faire un appel à l'API avec la fonction call API
+ * Afficher les visualisations avec la fonction generateCharts.
+ * @param {*} datas 
+ */
+async function onAddFile(event) {
+    
 }
 
 function prepareStringData(data) {
@@ -13,34 +18,18 @@ function prepareStringData(data) {
     return first.join('\n')
 }
 
-async function callOpenAI (strData) {
-    const response = await fetch("http://localhost:3001", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            data: strData
-        })
-    });
-    return await response.json()
+/**
+ * Cette fonction va récuperer une partie du contenu du fichier et faire une requête à l'API
+ * @param {*} datas 
+ */
+async function callApi (strData) {
 }
 
+/**
+ * Cette fonction va recuperer les resultats renvoyés par l'API
+ * Et génerer les charts sur la page html via chart.js
+ * @param {*} datas 
+ */
 function createCharts(datas) {
-    console.log(datas)
-    for(const chart of datas) {
-        const containerDiv = document.createElement('div')
-        containerDiv.className = 'container'
-        const title = document.createElement('h3')
-        title.innerText = chart.title
-        const div = document.createElement('div')
-        div.className = 'chart-container'
-        const canvas = document.createElement('canvas')
-        const ctx = canvas.getContext('2d')
-        new Chart(ctx, chart.chartjs)
-        div.appendChild(canvas)
-        containerDiv.appendChild(title)
-        containerDiv.appendChild(div)
-        document.body.appendChild(containerDiv)
-    }
+
 }
--- server/index.js
+++ server/index.js
@@ -15,46 +15,34 @@ app.use(cors({
 }))
 app.use(express.json());
 
-const messageContent = (data) => `
+/**
+ * Cette fonction va generer un prompt pour l'API OPEN AI avec les données envoyés par le navigateur
+ * @param {*} data 
+ * @returns 
+ */
+const generatePrompt = (data) => `
 You are a data analyst and a chart.js chart generator, you will answer with a following exact format.
 With this following data, you will generata a list of logic data charts that explains that will describe the data.
 You'll follow the rules of generating good datavisualisations
 Your response should be a JSON with double quotes only.
-Do not shortcut
-The response format: 
-[
-  {
-    "title": "Example Chart.js Configuration",
-    "chartjs": {
-      "type": "bar",
-      "data": {
-        "labels": ["Label1", "Label2", "Label3"],
-        "datasets": [{
-          "label": "",
-          "data": [10, 20, 30],
-          "backgroundColor": ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
-          "borderColor": ["rgba(255,99,132,1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
-          "borderWidth": 1,
-          "hoverBackgroundColor": "rgba(255, 99, 132, 0.4)",
-          "hoverBorderColor": "rgba(255, 99, 132, 1)",
-          "hoverBorderWidth": 2,
-          "stack": "Stack 0"
-        }]
-      }
-    }
-  }  
-]
+Do not shortcut the data
 
 Here is the data :
 ${data}
 `
 
-const generateMessage = (data) => [
-  { role: 'user', content: messageContent(data) },
+/**
+ * Cette fonction va appeller recevoir le prompt et generer le format de message attendu par l'api OPEN AI
+ * @param {*} data 
+ * @returns 
+ */
+const generateMessage = (prompt) => [
+  { role: 'user', content: prompt },
   { role: 'system', content: `
     [
       {
         "title": "Example Chart.js Configuration",
+        "description": "A description of what is represented in the data",
         "chartjs": {
           "type": "bar",
           "data": {
@@ -77,10 +65,15 @@ const generateMessage = (data) => [
   },
 ]
 
+/**
+ * Cette route va être appellée par le navigateur
+ * Elle va créer un appel vers l'api d'OPEN AI et recuperer le resultat et le renvoyer vers le navigateur
+ */
 app.post('/', async (req, res) => {
+  const prompt = generatePrompt(req.body.data)
   const response = await openai.chat.completions.create({
     model: "gpt-4",
-    messages: generateMessage(req.body.data),
+    messages: generateMessage(prompt),
     response_format: {
       type: "json_object"
     }
